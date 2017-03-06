8303868c3e87fe6a56db9dc3ab803d1fba7324af
Unnikrishnnan R <unnikrishnan.r@ammachilabs.org>
2017-03-06 14:56:39 +0530

    Added HMSGridTextCommand unity project (the version with board like grids and text input for moving the elephant about).

---
+++ HMSGridTextCommand/Assets/Script/CamFollowScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamFollowScript : MonoBehaviour 
+{
+    Vector3 camPosDisplacement;
+    public GameObject haathi;
+	// Use this for initialization
+	void Start () 
+    {
+        camPosDisplacement = new Vector3(0,5,-7);
+        //camPosDisplacement = new Vector3(-147, , -7);    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = haathi.transform.position + camPosDisplacement;
+	}
+}
