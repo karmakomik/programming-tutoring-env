0bc0f85f07b93e7a8f2bbf42b1b00d83efc4331c
karmakomik <unni.iceman@gmail.com>
2017-03-07 16:13:57 +0530

    Adding HMSGrid (version from last year with grid based movement and typed commands)

---
+++ HMSGrid/Assets/Script/GridScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Vectrosity;
+
+[ExecuteInEditMode]
+
+public class GridScript : MonoBehaviour 
+{
+    VectorLine line1, line2;
+    List<Vector3> pointsList1, pointsList2;
+    float startX = -120;
+    float startZ = -100;
+    float gridYHeight = 0.2f;
+    Material lineMat;
+    public Material lineMaterial1;
+
+	// Use this for initialization
+	void Start () 
+    {
+        pointsList1 = new List<Vector3>();
+        pointsList2 = new List<Vector3>();
+        for(int i = 0; i < 100; i++)
+        {
+            pointsList1.Add(new Vector3(startX + i*3, gridYHeight, startZ));
+            pointsList1.Add(new Vector3(startX + i * 3, gridYHeight, startZ + 300));
+            pointsList2.Add(new Vector3(startX, gridYHeight, startZ + i * 3));
+            pointsList2.Add(new Vector3(startX + 300, gridYHeight, startZ + i * 3));
+        }
+
+        line1 = new VectorLine("grid1", pointsList1, null, 0.3f, LineType.Discrete);
+        line1.AddNormals();
+        line2 = new VectorLine("grid2", pointsList2, null, 0.9f, LineType.Discrete);
+        line2.AddNormals();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        line1.Draw3D();
+        line2.Draw3D();
+	}
+}
---
+++ HMSGrid/Assets/Script/HaathiScript.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HaathiScript : MonoBehaviour 
+{
+    List<string> commsExecList = new List<string>(); //Commands to be executed
+    public float smoothFactor = 2; //smoothing factor for the elephant's movement
+	// Use this for initialization
+
+	void Start () 
+    {
+        Screen.orientation = ScreenOrientation.Portrait;
+	}
+    
+    void turnRightOps()
+    {
+        transform.Rotate(new Vector3(0, 1, 0), 90);
+    }
+
+    void turnLeftOps()
+    {
+        transform.Rotate(new Vector3(0, 1, 0), -90);
+    }
+
+    void goForwardOps()
+    {
+        transform.Translate(0, 0.0f, 3.0f);
+    }
+
+    void eatOps()
+    {
+        
+    }
+	// Update is called once per frame
+	void Update () 
+    {
+        if(commsExecList.Count > 0)
+        {
+            if (commsExecList[0].Equals("goforward();"))
+            {
+                goForwardOps();
+                //Vector3 targetPosition = ;
+                //transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smoothFactor);
+            }
+            else if (commsExecList[0].Equals("turnleft();"))
+            {
+                turnLeftOps();
+            }
+            else if (commsExecList[0].Equals("turnright();"))
+            {
+                turnRightOps();
+            }
+            else if (commsExecList[0].Equals("eat();"))
+            {
+                eatOps();
+            }
+            commsExecList.RemoveAt(0);
+        }
+	}
+
+    void addCommandToQueue(string comm)
+    {
+        commsExecList.Add(comm);
+    }
+}
